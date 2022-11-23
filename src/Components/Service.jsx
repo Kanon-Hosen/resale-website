cc512378358cfc352d11627dac37caee490d711d
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-11-23 23:36:52 +0600

    Service

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
-
+import {AiOutlineLike} from 'react-icons/ai'
+import {MdPayment} from 'react-icons/md'
+import {TbChartDots} from 'react-icons/tb'
 const Service = () => {
     return (
-        <div className='my-16'>
+        <div className='my-16 px-16'>
             <h1 className='text-4xl capitalize font-bold text-center'>What our serve for you</h1>
-            <p></p>
+            <p className='text-center my-4 text-gray-500'>We provide many of the best services for you and you will get the<br />  best benefits here</p>
+            <div className='mt-8 grid grid-cols-3 gap-8'>
+                <div className='shadow px-4 py-6 hover:bg-blue-50 transition-colors'>
+                    <div className='text-blue-500'>
+                    <AiOutlineLike className='text-5xl '></AiOutlineLike>
+
+                    </div>
+                    <div className='mt-4'>
+                        <h4 className='font-semibold text-xl text-slate-800 my-2 capitalize'>top buy & sell car</h4>
+                        <p>Buy and sell most trusted car, we provide the best platfrom for you and easy to use</p>
+                    </div>
+                </div>
+                <div className='shadow px-4 py-6 hover:bg-blue-50 transition-colors'>
+                    <div className='text-blue-500'>
+                    <MdPayment className='text-5xl '></MdPayment>
+
+                    </div>
+                    <div className='mt-4'>
+                        <h4 className='font-semibold text-xl text-slate-800 my-2 capitalize'>top buy & sell car</h4>
+                        <p>Buy and sell most trusted car, we provide the best platfrom for you and easy to use</p>
+                    </div>
+                </div>
+                <div className='shadow px-4 py-6 hover:bg-blue-50 transition-colors'>
+                    <div className='text-blue-500'>
+                    <TbChartDots className='text-5xl '></TbChartDots>
+
+                    </div>
+                    <div className='mt-4'>
+                        <h4 className='font-semibold text-xl text-slate-800 my-2 capitalize'>top buy & sell car</h4>
+                        <p>Buy and sell most trusted car, we provide the best platfrom for you and easy to use</p>
+                    </div>
+                </div>
+               
+            </div>
         </div>
     );
 };
--- src/MainRoute/MainRoute.js
+++ src/MainRoute/MainRoute.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Blog from '../Pages/Blog';
+import Dashboard from '../Pages/Dashboard';
 import Home from '../Pages/Home';
 import Layout from '../Pages/Layout';
+import Login from '../Pages/Login';
+import Register from '../Pages/Register';
+import Services from '../Pages/Services';
 const MainRoutes = () => {
     const router = createBrowserRouter([
         {
@@ -11,6 +16,26 @@ const MainRoutes = () => {
                 {
                     path: '/',
                     element:<Home></Home>
+                },
+                {
+                    path: '/services',
+                    element:<Services></Services>,
+                },
+                {
+                    path: '/blog',
+                    element:<Blog></Blog>
+                },
+                {
+                    path: '/login',
+                    element:<Login></Login>
+                },
+                {
+                    path: '/register',
+                    element:<Register></Register>
+                },
+                {
+                    path: '/dashboard',
+                    element:<Dashboard></Dashboard>
                 }
             ]
         }
